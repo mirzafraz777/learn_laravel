7b745295000086fdff6aee5c98c26f83f6505d60
Faraz Ahmad <mirzafraz777@gmail.com>
2024-06-04 16:34:09 +0500

    Google Spreadsheet API Task Complete and working fine.

--- app/Http/Controllers/GoogleSheetsController.php
+++ app/Http/Controllers/GoogleSheetsController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Services\GoogleSheetsService;
-use Illuminate\Http\Request;
 
 class GoogleSheetsController extends Controller
 {
@@ -16,45 +15,48 @@ class GoogleSheetsController extends Controller
 
     public function transferData()
     {
-        ini_set('max_execution_time', 60*12); // 3600 seconds = 60 minutes
-        set_time_limit(60*12);
-        
+        ini_set('max_execution_time', 60 * 12); // 3600 seconds = 60 minutes
+        set_time_limit(60 * 12);
+
 
         // Define your source and destination spreadsheet IDs and ranges
-        $sourceSpreadsheetId = '1OTtC26qjWFGwmjMuhDUqznVWNnLzrrm4-adlS5Im6Us';
+        // $sourceSpreadsheetId = '1OTtC26qjWFGwmjMuhDUqznVWNnLzrrm4-adlS5Im6Us'; // Test Source Sheet
+        $sourceSpreadsheetId = '12lNlY5_lZvgpw_uJ2FedF6eQiu0hZUarnosy25v-rxA'; // Main Source Sheet
         $sourceRange = [
-            'Real Sites - Blogger Outreach!B3:B10', // Adjust the range as needed
-            'Real Sites - Blogger Outreach!D3:L10', // Adjust the range as needed
-            'Real Sites - Blogger Outreach!N3:P10', // Adjust the range as needed
-            'Real Sites - Blogger Outreach!AA3:AB10', // Adjust the range as needed
-            'Real Sites - Blogger Outreach!AE3:AH10' // Adjust the range as needed
-        ]; 
+            'Real Sites - Blogger Outreach!B3:B',   // Adjust the range as needed
+            'Real Sites - Blogger Outreach!D3:L',   // Adjust the range as needed
+            'Real Sites - Blogger Outreach!N3:P',   // Adjust the range as needed
+            'Real Sites - Blogger Outreach!S3:T',   // Adjust the range as needed
+            'Real Sites - Blogger Outreach!AA3:AB', // Adjust the range as needed
+            'Real Sites - Blogger Outreach!AE3:AH',  // Adjust the range as needed
+        ];
 
-        $destinationSpreadsheetId = '19KSQVAK4zy8_b1a8wtohFkXBjrYcB7hCDUx7OXQBNjw';
+        // $destinationSpreadsheetId = '19KSQVAK4zy8_b1a8wtohFkXBjrYcB7hCDUx7OXQBNjw'; // Test Destination Sheet
+        $destinationSpreadsheetId = '1x03_01FerT9mYK8BDzCaWUSu_ZYd8Jrs8Qmf0i8oQ4s';    // Main Destination Sheet
+        // $destinationRange ='Test Sheet!B3'; 
         $destinationRanges = [
-            'Test Sheet!B3',
-            'Test Sheet!C3',
-            'Test Sheet!L3',
-            'Test Sheet!O3',
-            'Test Sheet!Q3',
+            'Price-Email!B2',
+            'Price-Email!C2',
+            'Price-Email!L2',
+            'Price-Email!O2',
+            'Price-Email!Q2',
+            'Price-Email!S2',
         ]; // Adjust the range as needed
 
         // Get data from the source spreadsheet
-        $sourceData = $this->googleSheetsService->getSpreadsheetData($sourceSpreadsheetId, $sourceRange);
+        $sourceValues = $this->googleSheetsService->batchGetValues($sourceSpreadsheetId, $sourceRange);
 
         // $values = [
         //     ['ABC','123@gmail.com'],['XYZ','=1+3']
         // ];
-       
-        // Prepare data for destination spreadsheet
-       $destinationData = [];
-       foreach ($sourceData as $index => $valueRange) {
-           $destinationData[$destinationRanges[$index]] = $valueRange->getValues();
-       }
+
         // Update the destination spreadsheet with the retrieved data
-       $body =  $this->googleSheetsService->updateSpreadsheet($destinationSpreadsheetId, $destinationData);
+           $destinationValues =  $this->googleSheetsService->batchUpdateValues($destinationSpreadsheetId, $destinationRanges,"RAW", $sourceValues);
+
 
         // return response()->json(['message' => 'Data transferred successfully']);
-        return response()->json($body);
+
+        // return response()->json($sourceValues);
+        return response()->json($destinationValues);
     }
 }
--- app/Services/GoogleSheetsService.php
+++ app/Services/GoogleSheetsService.php
@@ -6,7 +6,6 @@ use Google\Client;
 use Google\Service\Sheets;
 use Google\Service\Sheets\BatchUpdateValuesRequest;
 use Google\Service\Sheets\ValueRange;
-use Google\Service\Sheets\ClearValuesRequest;
 
 class GoogleSheetsService
 {
@@ -22,36 +21,69 @@ class GoogleSheetsService
         $this->service = new Sheets($this->client);
     }
 
-    public function getSpreadsheetData($spreadsheetId, $range)
+    public function batchGetValues($spreadsheetId, $givenRange)
     {
-        // $optparam = [
-        //     'majorDimension'=>'COLUMNS'
-        // ];
+        try {
 
-        $response = $this->service->spreadsheets_values->batchGet($spreadsheetId,['ranges'=>$range]);
-        return $response->getValueRanges();
-        // return $response;
+            $params = array(
+                'ranges' => $givenRange,
+                'majorDimension' => 'ROWS'
+
+            );
+            $result = $this->service->spreadsheets_values->batchGet($spreadsheetId, $params);
+
+            // printf("%d ranges retrieved.", count($result->getValueRanges()));
+            return $result->getValueRanges();
+        } catch (\Throwable $th) {
+            throw $th;
+            // TODO(developer) - handle error appropriately
+            // echo 'Message: ' .$e->getMessage();
+
+        }
     }
 
-    public function updateSpreadsheet($spreadsheetId, array $values)
+    public function batchUpdateValues($spreadsheetId, $destinationRanges, $valueInputOption,  $sourceValues)
     {
 
-        $data = [];
-        
-        foreach ($values as $range => $value) {
-            $data = new ValueRange([
-                'range'=>$range,
-                'values'=>$value
-            ]);
-        }
+        // $values = [
+        //     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        //     [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        //     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        // ];
+
+        // return $sourceValues;
+        // return $sourceValues[0]->values;
+        try {
+
+            $data = [];
+
+            foreach ($destinationRanges as $key => $destinationRange) {
+
+                $data[] = new ValueRange([
+                    'majorDimension' => 'ROWS', // "ROWS","COLUMNS"
+                    'range' => $destinationRange,
+                    'values' => $sourceValues[$key]->values
+                ]);
+
+
+            }
+
+
             $body = new BatchUpdateValuesRequest([
-            'valueInputOption' => 'USER_ENTERED',
-            'data'=>$data
+                'valueInputOption' => $valueInputOption, // "RAW","USER_ENTERED"
+                'data' => $data,
+                // 'includeValuesInResponse' => true,
+                'responseValueRenderOption' => 'FORMATTED_VALUE',  //"FORMATTED_VALUE","UNFORMATTED_VALUE","FORMULA"
+                'responseDateTimeRenderOption' => 'FORMATTED_STRING',  // "SERIAL_NUMBER","FORMATTED_STRING"
             ]);
 
-            return $body;
-
-
-        return $this->service->spreadsheets_values->batchUpdate($spreadsheetId,$body); // update($spreadsheetId, $range, $body, $params);
+            $result = $this->service->spreadsheets_values->batchUpdate($spreadsheetId, $body);
+            // printf("%d cells updated.", $result->getTotalUpdatedCells());
+            return $result;
+        } catch (\Throwable $th) {
+            throw $th;
+            // TODO(developer) - handle error appropriately
+            // echo 'Message: ' .$e->getMessage();
+        }
     }
 }
